de911247c287dd5a8a6938435782f7d629095270
102AMIT <kingston.amit9830@gmail.com>
2023-08-14 01:09:23 +0530

    implementing functionality for getAllorder,updateOrder and deleteOrder by admin

--- backend/controllers/orderController.js
+++ backend/controllers/orderController.js
@@ -65,3 +65,78 @@ exports.myOrders = catchAsyncErrors(async (req, res, next) => {
     orders,
   });
 });
+
+// get all orders access by admin
+
+exports.getAllOrders = catchAsyncErrors(async (req, res, next) => {
+  const orders = await Order.find();
+
+  let totalAmount = 0;
+
+  // show the total amount of all orders in admin dashboard
+  orders.forEach((order) => {
+    totalAmount += order.totalPrice;
+  });
+  res.status(200).json({
+    success: true,
+    orders,
+  });
+});
+
+// update order status / process order - ADMIN
+
+exports.updateOrder = catchAsyncErrors(async (req, res, next) => {
+  const order = await Order.findById(req.params.id);
+
+  if (order.orderStatus === "Delivered") {
+    return next(new ErrorHandler("You have already delivered this order", 400));
+  }
+
+  // update stock for each product in order (see productModel.js)
+
+  order.orderItems.forEach(async (item) => {
+    await updateStock(item.product, item.quantity);
+  });
+
+  order.orderStatus = req.body.status;
+
+  // if order is delivered, add deliveredAt date
+  if (req.body.status === "Delivered") {
+    order.deliveredAt = Date.now();
+  }
+
+  await order.save({ validateBeforeSave: false });
+
+  res.status(200).json({
+    success: true,
+  });
+});
+
+async function updateStock(id, quantity) {
+  const product = await Product.findById(id);
+
+  // update stock
+  product.stock = product.stock - quantity;
+
+  await product.save({ validateBeforeSave: false });
+}
+
+// delete order - ADMIN
+
+exports.deleteOrder = catchAsyncErrors(async (req, res, next) => {
+  const order = await Order.findById(req.params.id);
+
+  if (!order) {
+    // return next(new ErrorHandler("No order found with this ID", 404));
+    return res.status(404).json({
+      success: false,
+      message: "No order found with this ID",
+    });
+  }
+
+  await order.deleteOne();
+
+  res.status(200).json({
+    success: true,
+  });
+});
--- backend/routes/orderRoute.js
+++ backend/routes/orderRoute.js
@@ -1,11 +1,31 @@
 const express = require("express");
 const router = express.Router();
 const { isAuthenticatedUser, authorizeRoles } = require("../middleware/auth");
-const { newOrder, getSingleOrder, myOrders } = require("../controllers/orderController");
+const {
+  newOrder,
+  getSingleOrder,
+  myOrders,
+  getAllOrders,
+  updateOrder,
+  deleteOrder,
+} = require("../controllers/orderController");
 
 router.post("/order/new", isAuthenticatedUser, newOrder);
 
 router.get("/order/:id", isAuthenticatedUser, getSingleOrder);
 
 router.get("/orders/me", isAuthenticatedUser, myOrders);
+
+router.get(
+  "/admin/orders",
+  isAuthenticatedUser,
+  authorizeRoles("admin"),
+  getAllOrders
+);
+
+router.put('/admin/order/:id', isAuthenticatedUser, authorizeRoles('admin'), updateOrder)
+
+router.delete('/admin/order/:id', isAuthenticatedUser, authorizeRoles('admin'), deleteOrder)
+
+
 module.exports = router;
