f03b15f773ed5d54266e1a3d362e874a2405b801
102AMIT <kingston.amit9830@gmail.com>
2023-08-14 00:32:45 +0530

    getting single order and multiple order functionality done

--- backend/controllers/orderController.js
+++ backend/controllers/orderController.js
@@ -33,3 +33,35 @@ exports.newOrder = catchAsyncErrors(async (req, res, next) => {
     order,
   });
 });
+
+// get single order details
+
+exports.getSingleOrder = catchAsyncErrors(async (req, res, next) => {
+  // populate user field with name and email fields from User model (see orderModel.js)
+  const order = await Order.findById(req.params.id).populate(
+    // "user" is the field in orderModel.js that references the User model
+    "user",
+    // "name email" are the fields in the User model that we want to populate
+    "name email"
+  );
+
+  if (!order) {
+    return next(new ErrorHandler("No order found with this ID", 404));
+  }
+
+  res.status(200).json({
+    success: true,
+    order,
+  });
+});
+
+// get logged in user orders
+
+exports.myOrders = catchAsyncErrors(async (req, res, next) => {
+  const orders = await Order.find({ user: req.user._id });
+
+  res.status(200).json({
+    success: true,
+    orders,
+  });
+});
--- backend/routes/orderRoute.js
+++ backend/routes/orderRoute.js
@@ -1,8 +1,11 @@
 const express = require("express");
 const router = express.Router();
 const { isAuthenticatedUser, authorizeRoles } = require("../middleware/auth");
-const { newOrder } = require("../controllers/orderController");
+const { newOrder, getSingleOrder, myOrders } = require("../controllers/orderController");
 
 router.post("/order/new", isAuthenticatedUser, newOrder);
 
+router.get("/order/:id", isAuthenticatedUser, getSingleOrder);
+
+router.get("/orders/me", isAuthenticatedUser, myOrders);
 module.exports = router;
