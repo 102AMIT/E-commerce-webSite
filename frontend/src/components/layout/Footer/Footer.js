9b3f3388373ad103f91af295d0c5e8063f2a6f93
102AMIT <kingston.amit9830@gmail.com>
2023-08-14 16:58:28 +0530

    adding footer section with typeWriter

--- frontend/src/components/layout/Footer/Footer.js
+++ frontend/src/components/layout/Footer/Footer.js
@@ -1,23 +1,33 @@
-import React from 'react'
-import playStore from '../../../images/playStore.png'
-import appStore from '../../../images/appStore.png'
+import React from "react";
+import playStore from "../../../images/playStore.png";
+import appStore from "../../../images/appStore.png";
+import "./Footer.css";
+import TypewriterFooter from "./TypeWriter";
 const Footer = () => {
   return (
     <footer className="footer">
-        <div className="leftFooter">
+      <div className="leftFooter">
         <h4>Download Our APP</h4>
         <p>Download App for Android and IOS mobile phone</p>
         <img src={playStore} alt="playStore" />
         <img src={appStore} alt="appStore" />
-        </div>
+      </div>
 
-        <div className="middleFooter">
-        </div>
+      <div className="midFooter">
+        <h1>ECOMMERCE</h1>
+        <p>High quality is our first priority</p>
+        <p>Copyrights 2023 &copy; Amit Kumar Thakur</p>
+        <h3><TypewriterFooter /></h3>
+      </div>
 
-        <div className="rightFooter">
-        </div>
+      <div className="rightFooter">
+        <h4>Follow Us</h4>
+        <a href="https://www.linkedin.com/in/amit-thakur-ab7685128/">LinkedIn</a>
+        <a href="https://twitter.com/amit_thakur7766">Twitter</a>
+        <a href="https://github.com/102AMIT">GitHub</a>
+      </div>
     </footer>
-  )
-}
+  );
+};
 
-export default Footer
+export default Footer;
---
+++ frontend/src/components/layout/Footer/TypeWriter.js
@@ -0,0 +1,26 @@
+import React, { useState, useEffect } from "react";
+
+const TypewriterFooter = () => {
+  const text = "Created by Amit with ❤️ India";
+  const [displayText, setDisplayText] = useState("");
+  const [currentIndex, setCurrentIndex] = useState(0);
+
+  useEffect(() => {
+    const timer = setTimeout(() => {
+      if (currentIndex < text.length) {
+        setDisplayText(text.slice(0, currentIndex + 1));
+        setCurrentIndex(currentIndex + 1);
+      } else {
+        setCurrentIndex(0); // Reset currentIndex to restart the loop
+      }
+    }, 100); // Adjust the duration between each character reveal here
+
+    return () => clearTimeout(timer);
+  }, [currentIndex]);
+
+  return (
+      <h3 style={{height:"30px"}}>{displayText}</h3>
+  );
+};
+
+export default TypewriterFooter;
